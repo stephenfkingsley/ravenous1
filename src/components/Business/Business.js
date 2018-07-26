bd3261c5ac8cabd3aa82869b8a5dfa4702b3987e
Stephen Kingsley <stephenkingsley0@gmail.com>
2018-07-25 23:17:10 -0500

    First part of Ravenous part two where I am making the objects functional

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -26,16 +26,4 @@ class Business extends Component {
   }
 }
 
-let business = {
-imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-name: 'MarginOtto Pizzeria',
-address: '1010 Paddington Way',
-city: 'Flavortown',
-state: 'NY',
-zipCode: '10101',
-category: 'Italian',
-rating: 4.5,
-reviewCount: 90
-}
-
 export default Business
